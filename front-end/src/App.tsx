6aa6eab1afdb5aa4edac494815e4272d852b4f67
HuyNguyenUTA <huy.nguyen.uta@gmail.com>
2022-07-09 00:37:08 -0500

    added React Router v6

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -4,8 +4,9 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { TextField } from '@mui/material';
 import {createUseStyles} from 'react-jss';
-import Input from '../src/components/Input';
 import io from 'socket.io-client';
+import Room from './components/Room';
+import { Outlet, Link } from 'react-router-dom';
 
 const useStyles = createUseStyles({
   root: {
@@ -19,46 +20,47 @@ const useStyles = createUseStyles({
   }
 })
 
-const App: React.FC = () =>{
-  const classes = useStyles()
-  const [name, setName] = useState<string>("");
-  const [id, setID] = useState<number | undefined>();
-  const socket = io('http://localhost:3001', { transports : ['websocket'] })
+const socket = io('http://localhost:3001', { transports : ['websocket'] })
 
-  socket.on("connection", () => {})
+socket.on("connection", () => {})
 
-  const sendMessage = () => {
-    socket.emit("message", "Room created!")
+const App: React.FC = () => {
+    const classes = useStyles()
+    const [name, setName] = useState<string>("");
+    const [id, setID] = useState<number | undefined>();
+    
+    const sendMessage = () => {
+      socket.emit("message", "Room Created!")
+    }
+  
+    console.log(name)
+    console.log(id)
+  
+    return (
+      <div className={classes.root}>
+        <Typography variant='h2' align='center' className={classes.appName}>Chit Chat</Typography>
+  
+        <TextField
+          id="Name" 
+          label="Name" 
+          variant="outlined" 
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <TextField
+          id="id" 
+          label="id" 
+          variant="outlined" 
+          onChange={(e) => {
+            setID(parseInt(e.target.value));
+          }}
+        />
+
+        <Button variant="contained" component={Link} to='/room'>Enter</Button>
+      </div>
+    );
   }
-
-  console.log(name)
-  console.log(id)
-
-  return (
-    <div className={classes.root}>
-      <Typography variant='h2' align='center' className={classes.appName}>Chit Chat</Typography>
-
-      <TextField
-        id="Name" 
-        label="Name" 
-        variant="outlined" 
-        onChange={(e) => {
-              setName(e.target.value);
-        }}
-      />
-      <TextField
-        id="id" 
-        label="id" 
-        variant="outlined" 
-        onChange={(e) => {
-              setID(parseInt(e.target.value));
-        }}
-      />
-
-      <Button variant="contained" onClick={sendMessage}>Enter</Button>
-
-    </div>
-  );
-}
-
-export default App;
+  
+  export default App;
+  
--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Room from './components/Room';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+        <Route path='room' element={<Room />}/>
+    </Routes>
+  </BrowserRouter>
 );
 
